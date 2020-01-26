da638094f9ec76cfae4f39e5481e72464097c1c6
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-26 02:08:09 +0100

    Added styling to Text nodes

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  TextInput,
+  KeyboardAvoidingView,
+} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Welcome to React Native</Text>
+      <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
+      <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+      <Text style={[styles.largeText, styles.textStyle]}>
+        24 degree celsius
+      </Text>
     </View>
   );
 }
@@ -16,5 +26,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
   },
 });
